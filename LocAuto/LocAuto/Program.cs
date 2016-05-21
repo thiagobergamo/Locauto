6574cc4f6fc6a40f69f9842cf50fb1621f14e616
Thiago Bergamo <thiagobergamo@gmail.com>
2016-05-20 22:45:14 -0300

    Ajuste inicial

--- LocAuto/LocAuto/Program.cs
+++ LocAuto/LocAuto/Program.cs
@@ -16,7 +16,7 @@ namespace LocAuto
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Menu());
         }
     }
 }
