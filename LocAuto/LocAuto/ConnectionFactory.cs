e37fa515e4efd24c9c3bc09fe2214c52ed242d8a
Thiago Bergamo <thiagobergamo@gmail.com>
2016-05-21 11:43:58 -0300

    Criação da connectionFactory

---
+++ LocAuto/LocAuto/ConnectionFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient
+
+
+namespace LocAuto
+{
+    class ConnectionFactory
+    {
+        public MySqlConnection ObterConexao()
+        {
+            String str = @"server=localhost;database=locauto;userid=thiago;password=senha;";
+            MySqlConnection conn = new MySqlConnection(str);
+            return conn;
+        }
+}
