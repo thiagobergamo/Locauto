d7af414142c8553dab4e15c847acb0c5cec6fdeb
Thiago Bergamo <thiagobergamo@gmail.com>
2016-05-20 22:39:48 -0300

    Teste Alteração

--- LocAuto/LocAuto/Form1.Designer.cs
+++ LocAuto/LocAuto/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(154, 88);
+            this.button1.Location = new System.Drawing.Point(60, 91);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
