90686b8c5c8b18a8ca04ad6640b4e6f388301c0f
silvia_mayara <silvia_mayara@hotmail.com>
2016-05-21 12:16:24 -0300

    Criação das Classes. TipoVeiculo, Usuario, Veiculo

---
+++ LocAuto/LocAuto/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocAuto
+{
+    class Usuario
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+    }
+}
