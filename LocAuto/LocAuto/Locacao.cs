74532023f10dd4faa6fa73506ffa738f6de473b6
silvia_mayara <silvia_mayara@hotmail.com>
2016-05-21 11:33:11 -0300

    Criação das Classes. Locacao, Opcional, SituacaoVeiculo e TipoTelefone

---
+++ LocAuto/LocAuto/Locacao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocAuto
+{
+    class Locacao
+    {
+        public int Codigo { get; set; }
+        public string DataLocacao { get; set; }
+        public string DataPrevDevolucao { get; set; }
+        public string DataDevolucao { get; set; }
+        public string DormaPagamento { get; set; }
+        public decimal ValorTotal { get; set; }
+        public decimal Pago { get; set; }
+        public decimal ValorCaucao { get; set; }
+    }
+}
