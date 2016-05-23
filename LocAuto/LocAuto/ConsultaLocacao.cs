f7a033b3e1bdfe5be230c7a0dc01c1941072424e
nayara1 <nayara.julio@gmail.com>
2016-05-22 21:26:48 -0300

    Commit

---
+++ LocAuto/LocAuto/ConsultaLocacao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LocAuto
+{
+    public partial class ConsultaLocacao : Form
+    {
+        public ConsultaLocacao()
+        {
+            InitializeComponent();
+        }
+
+        private void ConsultaLocacao_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
