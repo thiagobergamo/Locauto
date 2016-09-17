cddebe0f6c5cf13def14f28ce724cea14ee3be42
José Mateus <mateusjoseph@gmail.com>
2016-09-17 14:30:36 -0300

    Criação classe PessoJuridaDAO

---
+++ LocAuto/DaoMysql/PessoaJuridicaDAO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo;
+using DaoInterface;
+
+namespace DaoMysql
+{
+    public class PessoaJuridicaDAO : IPessoaJuridicaDAO
+    {
+        public void inserir(PessoaJuridica pessoaJuridica, List<TelefoneCliente> telefoneCliente)
+        {
+
+        }
+
+        public PessoaJuridica Retornar(int codigo)
+        {
+            PessoaJuridica pessoaJuridica = new PessoaJuridica();
+            return pessoaJuridica;
+        }
+    }
+}
