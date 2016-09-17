3631a281fd7ae62a57256ca6209e8f3e203b3a71
José Mateus <mateusjoseph@gmail.com>
2016-09-17 14:58:56 -0300

    Correção visibilidade da classe TipoTelefoneDAO

---
+++ LocAuto/DaoMysql/TipoTelefoneDAO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaoMysql
+{
+    public class TipoTelefone
+    {
+        public void inserir(TipoTelefone tipoTelefone)
+        {
+
+        }
+    }
+}
