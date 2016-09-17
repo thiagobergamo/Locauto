2ebd181957b4f05ddfbe0cbf069d432301517a75
José Mateus <mateusjoseph@gmail.com>
2016-09-17 11:35:47 -0300

    Criação Classe Cliente

---
+++ LocAuto/Modelo/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public abstract class Cliente
+    {
+        public int Codigo { get; set; }
+        public string Email { get; set; }
+        public string LoginWeb { get; set; }
+        public string SenhaWeb { get; set; }
+        public string Cnh { get; set; }
+        public string ValidadeCnh { get; set; }
+        public string Logradouro { get; set; }
+        public int Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cep { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+    }
+}
