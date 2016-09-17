f1b332569f5f0d434f0971decb10305e3174ecdd
nayara1 <nayara.julio@gmail.com>
2016-09-17 11:44:00 -0300

    criação classes modelo

---
+++ LocAuto/Modelo/Locacao.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Locacao
+    {
+        public int Codigo { get; set; }
+        public int CodigoCliente { get; set; }
+        public int CodigoVeiculo { get; set; }
+        public string DataLocacao { get; set; }
+        public string DataPrevDevolucao { get; set; }
+        public string DataDevolucao { get; set; }
+        public string FormaPagamento { get; set; }
+        public decimal ValorTotal { get; set; }
+        public decimal ValorOpc { get; set; }
+        public int MyProperty { get; set; }
+        public string Pago { get; set; }
+        public decimal ValorCaucao { get; set; }
+    }
+}
