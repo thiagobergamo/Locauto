c5540b8cd8302a62900ceef52b5fe55f573dc783
nayara1 <nayara.julio@gmail.com>
2016-09-17 14:23:55 -0300

    Criação classe DAOMYSQL

---
+++ LocAuto/DaoMysql/TipoVeiculoDAO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo;
+using DaoInterface;
+
+namespace DaoMysql
+{
+    public class TipoVeiculoDAO : ITipoVeiculoDAO
+    {
+        public void inserir(TipoVeiculo tipoVeiculo)
+        {
+
+        }
+    }
+}
