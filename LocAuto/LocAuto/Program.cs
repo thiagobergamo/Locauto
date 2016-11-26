5164965709fef27c5de591faf7d2835c07bc140d
nayara1 <nayara.julio@gmail.com>
2016-11-26 18:21:12 -0200

    commit

--- LocAuto/LocAuto/Program.cs
+++ LocAuto/LocAuto/Program.cs
@@ -16,7 +16,7 @@ namespace LocAuto
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ConsultaPessoaJuridica());
+            Application.Run(new Menu());
         }
     }
 }
